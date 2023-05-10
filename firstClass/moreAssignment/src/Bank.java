ab54082e945b8007222526cb12f05ff9dee8fcf3
jinchiim <nellroll@naver.com>
2023-05-10 22:29:54 +0900

    [fix] Bank 코드리뷰 반영 commit
    [fix] Bank 코드리뷰 반영 commit
    
    #1

--- firstClass/moreAssignment/src/Bank.java
+++ firstClass/moreAssignment/src/Bank.java
@@ -2,15 +2,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Bank {
-    Scanner scanner = new Scanner(System.in);          // 값을 받아오고, 지정해주는 면에서 편해서 사용
-    Random random = new Random();                       // 랜덤인 계좌를 만들기 위해 import
     private static Service[] accounts = new Service[20]; // 리스트 생성
     private static int index = 0;
 
     public static int checkAccountNumber(String accountNumber) {
         for(int i=0; i<index; i++){
-                if(accounts[i].getAccountNumber().contentEquals(accountNumber)){        // 받은 계좌와, getAccountNumber()에서 가져온 계좌가 일치하는지 확인.
-                    return i;
+            if(accounts[i].getAccountNumber().contentEquals(accountNumber)){        // 받은 계좌와, getAccountNumber()에서 가져온 계좌가 일치하는지 확인.
+                return i;
             }
         } return -1;
     }
@@ -26,24 +24,24 @@ public class Bank {
         Scanner scanner = new Scanner((System.in));
         String accountNumber = (random.nextInt(90000) + 10000 ) + "-" + (random.nextInt(90000) + 10000 );   // 무작위 계좌
         System.out.println("계좌번호: " + accountNumber);                                 // 계좌는 임의로 생성 후 바로 사용자에게 보여줌.
-        System.out.println("고객이름: ");                                                 // 나머지는 scanner을 통해 입력 받은 값 사용.
+        System.out.print("고객이름: ");                                                 // 나머지는 scanner을 통해 입력 받은 값 사용.
         String name = scanner.nextLine();
-        System.out.println("비밀번호: ");
+        System.out.print("비밀번호: ");
         String password = scanner.nextLine();
         int budget = 0;                                                                 // 입금이 되어있는 상태가 아니기 때문에 0으로 시작.
-        System.out.println("금액: "+budget);
+        System.out.print("금액: "+budget);
 
         accounts[index] = new Service(accountNumber, password, name, budget);
         index ++;                                                                       // 새로운 계좌가 생길때마다 index++
 
         System.out.println(accountNumber + "인 계좌 계설 완료!");
 
-        }
+    }
 
     public static void findAccount() {
-        Scanner scanner = new Scanner((System.in));
-        System.out.println("계좌번호를 입력해주세요.");
-        String accountNumber = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("계좌번호를 입력해주세요.");
+        String accountNumber = sc.nextLine() ;
 
         int index = checkAccountNumber(accountNumber);                                  // 함수를 통해 위에서 받은 accountNumber가 일치하는지 확인.
         if (index == -1) {
@@ -53,14 +51,14 @@ public class Bank {
         }
 
 
-        }
+    }
 
     public static void moneyIn() {
         Scanner scanner = new Scanner((System.in));
 
-        System.out.println(("계좌번호를 입력해주세요.: "));
+        System.out.print(("계좌번호를 입력해주세요.: "));
         String accountNumber = scanner.nextLine();
-        System.out.println("입금할 금액: ");
+        System.out.print("입금할 금액: ");
         int money = scanner.nextInt();
         int index = checkAccountNumber(accountNumber);                                      // 위 와 일치.
         if (index == -1) {
@@ -73,23 +71,19 @@ public class Bank {
 
     public static void moneyOut() {
         Scanner scanner = new Scanner((System.in));
-        System.out.println("계좌번호를 입력해주세요.");
+        System.out.print("계좌번호를 입력해주세요.");
         String accountNumber = scanner.nextLine();
-        System.out.println("비밀번호 확인이 필요합니다.: ");
+        System.out.print("비밀번호 확인이 필요합니다.: ");
         String password = scanner.nextLine();
-        System.out.println("출금할 금액: ");
-        int money = scanner.nextInt();
         int index = checkAccountNumber(accountNumber);
         if (index == -1) {
             System.out.println("계좌가 존재하지 않습니다.");
         } else {
             if (!accounts[index].getPassword().equals(password)) {                  // 계좌가 존재하기는 하나 출금을 위해 비밀번호 확인
                 System.out.println("비밀번호가 일치하지 않습니다.");
-                return;
+
             } else {
-                int budget = accounts[index].getBudget();
-                accounts[index].withdraw(money);
-                System.out.printf("%d원이 인출되었습니다.%n", money-budget);
+                accounts[index].withdraw();
             }
         }
 
@@ -97,23 +91,21 @@ public class Bank {
 
     public static void changePassword(){
         Scanner scanner = new Scanner((System.in));
-        System.out.println("계좌번호를 입력해주세요.");
+        System.out.print("계좌번호를 입력해주세요.");
         String accountNumber = scanner.nextLine();
         int index = checkAccountNumber(accountNumber);
         if (index == -1) {
             System.out.println("계좌가 존재하지 않습니다.");
         } else {
-            System.out.println("이전 비밀번호를 입력해주세요.: ");                           // 일치할시만 통과
+            System.out.print("이전 비밀번호를 입력해주세요.: ");                           // 일치할시만 통과
             String password = scanner.nextLine();
             if (!accounts[index].getPassword().equals(password)) {
                 System.out.println("비밀번호가 일치하지 않습니다.");
-                return;
             } else {
-                System.out.println("바꾸고 싶은 비밀번호를 입력해주세요.: ");
+                System.out.print("바꾸고 싶은 비밀번호를 입력해주세요.: ");
                 String afterPassword = scanner.nextLine();
                 accounts[index].setPassword(afterPassword);                             // 새로 받은 비밀번호를 this.password = password를 통해 재정의
                 System.out.println("비밀번호를 변경 했습니다.");
-                return;
             }
         }
     }
